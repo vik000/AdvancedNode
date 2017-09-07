62c2644604c0eb1ee6ae1c386c124f5bed5d3a87
Javi Miguel <jamg44@gmail.com>
2017-09-07 21:31:46 +0200

    version de todos los modulos

--- ejemplos/version_modulos/index.js
+++ ejemplos/version_modulos/index.js
@@ -1,11 +1,52 @@
 "use strict";
 
 const versionModulo = require('./versionModulo');
+const fs = require('fs');
+const async = require('async');
 
-versionModulo('chance', (error, version) => {
-  if (error) {
-    console.log('Hubo un error', error);
+function versionModulos(callback) {
+  // leer contenido de node_modules
+  fs.readdir('./node_modules', (err, lista) => {
+    if (err) {
+      callback(err);
+      return;
+    }
+    console.log(lista);
+    // sacar nombre y version de cada modulo
+    async.concat(lista, function iterador(item, callbackIteracionEnCurso) {
+
+      // excluimos la carpeta .bin y otros posibles ficheros ocultos
+      if (item[0] === '.') { // si la primera letra es un '.'
+        process.nextTick(() => { // no hacemos nada, y metemos la 
+                                 // llamada en la siguiente vuelta del event loop
+          callbackIteracionEnCurso(null);
+        });
+        return;
+      }
+
+      // ya tenemos el nombre del modulo, recogemos su versión
+      versionModulo(item, (error, version) => {
+        if (error) {
+          callbackIteracionEnCurso(error);
+          return;
+        }
+        // no ha fallado, devolvemos el nombre del modulo y la version
+        callbackIteracionEnCurso(null, {item, version});
+      });
+
+    }, callback);
+  });
+
+
+  // devolver la lista de modulos
+}
+
+
+versionModulos((err, listaModulos) => {
+  if (err) {
+    console.log('Hubo un error', err);
     return;
   }
-  console.log('La versión del modulo chance es', version);
-})
+  console.log(listaModulos);
+});
+
